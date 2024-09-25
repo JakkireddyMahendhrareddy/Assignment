14e847062ec8edca3fd9054599f2971b65f68ff0
Jakkireddy Mahendrareddy <jakkireddymahendhrareddy@gmail.com>
2024-09-25 13:08:14 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,26 +1,4 @@
-// const express =require("express")
-// const mongoose =require('mongoose')
-// const cors= require("cors")
-// const EmployeeModel = require('./models/Employee')
 
-// const app =express()
-// app.use(express.json())
-// app.use(cors())
-
-// mongoose.connect("mongodb://127.0.0.1:27017/employee");
-
-// app.post('/register', (req, res) => {
-//     EmployeeModel.create(req.body)
-//     .then(employees => res.json(employees))
-//     .catch(err => res.json(err))
-
-// })
-
-// app.listen(3001, () => {
-
-// console.log("server is running")
-
-// })
 
 const express = require("express");
 const mongoose = require("mongoose");
@@ -31,7 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Connect to MongoDB
 mongoose.connect("mongodb://127.0.0.1:27017/employee", { 
     useNewUrlParser: true,
     useUnifiedTopology: true 
@@ -43,7 +20,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/employee", {
     console.error("MongoDB connection error:", err);
 });
 
-// API to register a new employee
+
 
 app.post('/login', (req, res) => {
     const {email,password}=req.body;
@@ -73,7 +50,7 @@ app.post('/register', (req, res) => {
     .catch(err => res.status(500).json({ error: err.message }));
 });
 
-// Start the server
+
 app.listen(3001, () => {
     console.log("Server is running on port 3001");
 });
